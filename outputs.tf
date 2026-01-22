1833cb4645e6b78395ff05abe897b2a855f282ff
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2026-01-22 15:54:21 -0500

    Define outputs and remove unneeded outputs inherited from skeleton

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,14 @@
-output "arn" {
-  description = "The EC2 instance ARN."
-  value       = aws_instance.example.arn
+output "completion_report_bucket" {
+  description = "The name of the S3 bucket where cost and usage completion reports are stored."
+  value       = aws_s3_bucket.completion_report
 }
 
-output "availability_zone" {
-  description = "The AZ where the EC2 instance is deployed."
-  value       = aws_instance.example.availability_zone
+output "data_export_bucket" {
+  description = "The name of the S3 bucket where cost and usage data exports are stored."
+  value       = aws_s3_bucket.export
 }
 
-output "id" {
-  description = "The EC2 instance ID."
-  value       = aws_instance.example.id
-}
-
-output "private_ip" {
-  description = "The private IP of the EC2 instance."
-  value       = aws_instance.example.private_ip
-}
-
-output "subnet_id" {
-  description = "The ID of the subnet where the EC2 instance is deployed."
-  value       = aws_instance.example.subnet_id
+output "replication_role_arn" {
+  description = "The ARN of the IAM role that can be assumed to perform replication of CUR data export reports."
+  value       = aws_iam_role.replication_role.arn
 }
