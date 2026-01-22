e6b4addd83d04a9a3b44d8ce5d920f4da79692fe
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2026-01-22 15:42:54 -0500

    Define initial set of local variables

---
+++ locals.tf
@@ -0,0 +1,20 @@
+# ------------------------------------------------------------------------------
+# Retrieve the effective Account ID, User ID, and ARN in which Terraform is
+# authorized.  This is used to calculate the session names for assumed roles.
+# ------------------------------------------------------------------------------
+data "aws_caller_identity" "current" {}
+
+data "aws_caller_identity" "master" {
+  provider = aws.master
+}
+
+# ------------------------------------------------------------------------------
+# Evaluate expressions for use throughout this configuration.
+# ------------------------------------------------------------------------------
+locals {
+  # Extract the user name of the current caller for use
+  # as assume role session names.
+  caller_user_name = split("/", data.aws_caller_identity.current.arn)[1]
+
+  master_account_id = data.aws_caller_identity.master.account_id
+}
